18a1d1415987b4697811294c56d7f6a3e6b5507f
nadaghorghar <162602751+nadaghorghar@users.noreply.github.com>
2025-11-26 00:32:01 +0100

    Rename parameter in executer_politique function

--- src/menu.h
+++ src/menu.h
@@ -1,12 +1,10 @@
 #ifndef MENU_H
 #define MENU_H
 
-// Inclure process.h pour connaître le type Process
 #include "process.h"
 
-// Fonctions du menu
 void afficher_policies();
 int choisir_politique();
-void executer_politique(int choix, Process procs[], int count);
+void executer_politique(int index, Process procs[], int count);
 
 #endif
