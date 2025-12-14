417837205d9e2f52cc1cb1519bb8743cd453c0a1
Hiba Wakel <130706361+hibabab@users.noreply.github.com>
2025-12-14 18:45:10 +0100

    Update menu.h

--- src/menu.h
+++ src/menu.h
@@ -3,16 +3,16 @@
 
 #include "process.h"
 
-// Charger les politiques disponibles
+
 void charger_politiques(void);
 
-// Afficher les politiques disponibles
+
 void afficher_policies(void);
 
-// Choisir une politique
+
 int choisir_politique(void);
 
-// Exécuter la politique choisie
+
 void executer_politique(int index, Process procs[], int count);
 
 #endif
