ac2f13e41e39cc918ec5f3f6e50a9f6aba5169e6
Hiba Wakel <130706361+hibabab@users.noreply.github.com>
2025-12-14 18:41:12 +0100

    Refactor SchedulingResult structure in gui.h

--- src/gui.h
+++ src/gui.h
@@ -3,9 +3,9 @@
 
 #include "process.h"
 
-// Structure pour stocker les résultats d'exécution
+
 typedef struct {
-    int timeline[2000];      // Augmenté pour Multi-Level
+    int timeline[2000];      
     int timeline_len;
     int start[MAXP];
     int end[MAXP];
@@ -14,19 +14,17 @@ typedef struct {
     float avg_turnaround;
     float avg_wait;
     int process_count;
-    Process *processes;      // Pointeur vers les processus triés
-    int quantum;             // Pour Round Robin
-    char algo_name[50];      // Nom de l'algorithme utilisé
+    Process *processes;      
+    int quantum;             
+    char algo_name[50];      
 } SchedulingResult;
 
-// Variables globales pour capturer les résultats
 extern SchedulingResult *current_result;
 extern int capture_mode;
 
-// Fonction principale pour lancer l'interface GTK
 void lancer_interface_gtk(Process procs[], int count);
 
-// Déclarations des fonctions d'ordonnancement
+
 void fifo(Process procs[], int n);
 void round_robin(Process procs[], int n, int quantum);
 void priorite(Process procs[], int n);
